fe3d7b76a379335b32ffebc6b783834b91dbd971
STnobile <stnobile0@gmail.com>
2023-05-25 12:23:22 +0000

    fixed heroku host name in setting.py

--- pp4/settings.py
+++ pp4/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['doc-app.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['doc-app.herokuapp.com', 'localhost']
 
 
 # Application definition
