1a6443627c4c4a09d4d70cb62d34b0d55d10f48a
STnobile <stnobile0@gmail.com>
2023-06-08 16:39:19 +0000

    Add the allauth library and import the accounts, set up the login,logout,signup html everything is working fine, having some issues on using my personal templates. working on.

--- pp4/urls.py
+++ pp4/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('app_doc.urls'))
+    path("", include('app_doc.urls')),
+    path('accounts', include('allauth.urls')),
 ]
