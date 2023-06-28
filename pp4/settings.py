e46a990cef9b22d5f757a8c394e8fad4f2c9b6d5
STnobile <stnobile0@gmail.com>
2023-06-28 23:56:07 +0000

    debug to false

--- pp4/settings.py
+++ pp4/settings.py
@@ -157,13 +157,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Sending Emails
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = ('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = ('EMAIL_HOST_PASS')
-# DEFAULT_FROM_EMAIL = ('EMAIL_HOST_USER')
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'test.email@gmail.com'
